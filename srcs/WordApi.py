70603710a24d3795d5680c81aa5d3860dd83fe34
schuah <chuahtseyung2002@gmail.com>
2023-04-09 23:22:30 +0800

    Bot can add audio to channel now

--- srcs/WordApi.py
+++ srcs/WordApi.py
@@ -2,7 +2,6 @@ from gtts import gTTS
 from bs4 import BeautifulSoup
 import requests
 import random
-import json
 import os
 
 URL			= 'https://www.thefreedictionary.com/'
@@ -16,35 +15,34 @@ class WordApi:
 			raise ValueError('No English words found in ' + WORDS_PATH)
 		self.word = None
 		self.definition	= None
-		self.sentence = None
+		self.characters = []
 	
 	def generate_word(self):
-		while True:
+		while self.word is None and self.definition is None :
 			try:
 				self.word = random.choice(self.english_words).lower()
+				self.characters = random.sample(self.word, len(self.word) // 2)
 				response = requests.get(URL + self.word)
 				if (response.status_code == 200):
 					soup = BeautifulSoup(response.text, 'html.parser')
-					extracted = soup.find('div', {'class': 'ds-list'}).text.strip().split(':')
-					self.definition = "".join([extracted[0], extracted[0][:-1]][extracted[0][-1] == '.'].split('.')[-1].strip())
-					break
+					extracted = soup.find('div', {'class': 'ds-list'})
+					if extracted:
+						extracted = extracted.text.strip().split(':')
+						self.definition = "".join([extracted[0], extracted[0][:-1]][extracted[0][-1] == '.'].split('.')[-1].strip())
+						tts = gTTS(text=self.word, lang='en')
+						tts.save(self.word + '-audio.mp3')
 				else:
 					print(f'Failed to retrieve the definition for {self.word}: {response.status_code}')
-			except Exception as err:
-				print(f'Error: {err}: {self.word}')
+			except Exception as e:
+				print(f'Failed to retrieve the definition for {self.word}: {e}')
 	
 	def generate_definition(self):
 		tts = gTTS(text=self.definition, lang='en')
 		tts.save(self.word + '-definition.mp3')
 	
-	def generate_sentence(self):
-		tts = gTTS(text=self.sentence, lang='en')
-		tts.save(self.word + '-sentence.mp3')
-
 	def cleanup(self):
 		try:
 			os.remove(self.word + '-audio.mp3')
 			os.remove(self.word + '-definition.mp3')
-			os.remove(self.word + '-sentence.mp3')
-		except FileNotFoundError():
+		except FileNotFoundError:
 			pass
--- srcs/events_utils.py
+++ srcs/events_utils.py
@@ -46,11 +46,14 @@ async def start_game(message):
 	if not channel:
 		return
 	await channel.send(f"Welcome to Speechle! You will be given an audio file to listen and your goal is to transcribe it into text.\n```MARKDOWN\n# Important information\n-> Your time limit is 30 seconds per word\n-> Max amount of points you can get from each word is 3 points\n-> Each hint you use reduces the points by 1\n-> You only get one attempt per word\n# Hints\n-> Definition: Provides a definition of the word\n-> Example: Uses the word in a sentence\n```\nScore as high as you can! Good luck and have fun!")
-	# wordApi = WordApi()
-	# await channel.send("Generating word...")
-	# wordApi.generate_word()
-	# print(wordApi.word)
-	# with open (wordApi.word + '-audio.mp3', 'rb') as f:
-	# 	file = discord.File(f, filename='audio.mp3')
-	# 	await channel.send(file=file)
-	# wordApi.cleanup()
+	try:
+		wordApi = WordApi()
+		await channel.send("Generating word...")
+		wordApi.generate_word()
+		with open (wordApi.word + '-audio.mp3', 'rb') as f:
+			file = discord.File(f, filename='audio.mp3')
+			await channel.send(file=file)
+		await channel.send(f"Word: {wordApi.word}\nDefinition: {wordApi.definition}\nCharacters: {wordApi.characters}")
+		wordApi.cleanup()
+	except Exception as e:
+		print(e)
