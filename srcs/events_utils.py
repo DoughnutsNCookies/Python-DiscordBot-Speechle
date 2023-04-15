c4c1c1c52d8df259aee368d46edde2d9bf0ef34e
schuah <chuahtseyung2002@gmail.com>
2023-04-15 12:39:14 +0800

    Added Timer

--- srcs/events_utils.py
+++ srcs/events_utils.py
@@ -6,6 +6,7 @@ import time
 import re
 
 CATEGORY_NAME = "Speechle"
+TIMEOUT = 10
 
 async def create_channel(message):
 	channel_name = re.sub(r"[^a-z]+", "", message.author.name.lower()) + "-" + message.author.discriminator
@@ -44,6 +45,16 @@ async def start_game(bot, message):
 		await sentView.edit(view=myView)
 		await channel.send(content)
 
+	# async def print_time(myView, sentEmbed, channel):
+	# 	startTime = time.monotonic()
+	# 	endTime = startTime + TIMEOUT
+	# 	while time.monotonic() < endTime:
+	# 		remainingTime = endTime - time.monotonic()
+	# 		embed = discord.Embed(title="Time left: " + str(round(remainingTime, 2)) + " seconds")
+	# 		await sentEmbed.edit(embed=embed)
+	# 		print(remainingTime)
+	# 		await asyncio.sleep(1)
+
 	channel_name = re.sub(r"[^a-z]+", "", message.author.name.lower()) + "-" + message.author.discriminator
 	guild = message.guild
 
@@ -53,7 +64,7 @@ async def start_game(bot, message):
 	channel = discord.utils.get(category.text_channels, name=channel_name)
 	if not channel:
 		return
-	await channel.send(f"**Welcome to Speechle!** You will be given an audio file to listen and your goal is to transcribe it into text.\n```MARKDOWN\n# Important\n-> Your time limit is 30 seconds per word\n-> You only get 1 attempt per word\n-> Max amount of points you can get from each word is 3 points\n# Points\n-> 1 point for a correct transcription\n-> 1 point for a correct transcription under 10 seconds\n-> 1 point for not using hint\n# Hints\n-> Definition: Provides a definition of the word\n```Score as high as you can! Good luck and have fun!")
+	await channel.send(f"**Welcome to Speechle!** You will be given an audio file to listen and your goal is to transcribe it into text.\n```MARKDOWN\n# Important\n-> Your time limit is 30 seconds per word\n-> You only get 1 attempt per word\n-> Max amount of points you can get from each word is 3 points\n# Points\n-> 1 point for a correct transcription\n-> 1 point for a correct transcription under 15 seconds\n-> 1 point for not using hint\n# Hints\n-> Definition: Provides a definition of the word\n```Score as high as you can! Good luck and have fun!")
 	try:
 		wordApi = WordApi()
 		while True:
@@ -65,16 +76,18 @@ async def start_game(bot, message):
 			sentView = await channel.send(file=file, view=myView)
 			wordApi.cleanup()
 			await channel.send(f"Word: {wordApi.word}\nDefinition: {wordApi.definition}\n")
-
 			try:
-				start_time = time.monotonic()
-				message = await bot.wait_for('message', check=lambda received: received.author == message.author and received.channel == channel, timeout=30)
-				duration = time.monotonic() - start_time
+				startTime = time.perf_counter()
+				sentEmbed = await channel.send(embed=discord.Embed(title=f"Time runs out <t:{int(time.time()) + TIMEOUT}:R>", color=discord.Color.blurple()))
+				message = await bot.wait_for('message', check=lambda received: received.author == message.author and received.channel == channel, timeout=(TIMEOUT))
 			except asyncio.TimeoutError:
-				return await update_message(myView, sentView, channel, "**Time's up!** The word was ``" + wordApi.word + "``. Better luck next time! Type ''s!start'' outside of this room to play again to start a new game!")
+				await sentEmbed.edit(embed=discord.Embed(title=f"Final time: {TIMEOUT}.00 seconds", color=discord.Color.red()))
+				return await update_message(myView, sentView, channel, "**Time's up!** The word was ``" + wordApi.word + "``. Better luck next time! Type ``s!start`` outside of this room to play again to start a new game!")
 			if message.content == wordApi.word:
+				await sentEmbed.edit(embed=discord.Embed(title=f"Final time: {round(time.perf_counter() - startTime, 2)} seconds", color=discord.Color.green()))
 				await update_message(myView, sentView, channel, "**Correct!**")
 			else:
+				await sentEmbed.edit(embed=discord.Embed(title=f"Final time: {round(time.perf_counter() - startTime, 2)} seconds", color=discord.Color.red()))
 				return await update_message(myView, sentView, channel, "**Incorrect!** The word was ``" + wordApi.word + "``. Better luck next time!\nType ``s!start`` outside of this room to play again to start a new game!")
 	except Exception as e:
 		print(e)
